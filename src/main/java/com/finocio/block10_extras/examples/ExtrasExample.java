62c81bd415172d23bd40b64b9075772732e20c43
Jaico Jara <practicante1finocio@gmail.com>
2026-01-06 16:26:25 +0100

    feat: add course extras and complete examples

---
+++ src/main/java/com/finocio/block10_extras/examples/ExtrasExample.java
@@ -0,0 +1,10 @@
+package com.finocio.block10_extras.examples;
+
+public class ExtrasExample {
+
+    public static void main(String[] args) {
+
+        Extras.test();
+        System.out.println(Extras.globalName);
+    }
+}
---
+++ src/main/java/com/finocio/block10_extras/exercises/Exercise03.java
@@ -0,0 +1,18 @@
+package com.finocio.block10_extras.exercises;
+
+/**
+ * Exercise03:
+ * Declara una constante final llamada MAX_SCORE con valor 100 y muéstrala.
+ */
+public class Exercise03 {
+    public static final int MAX_SCORE = 100;
+
+    public static void main(String[] args) {
+        System.out.println("La puntuación máxima es: " + MAX_SCORE);
+    }
+}
+/*
+Explicación:
+- final hace que la variable no se pueda modificar.
+- Las constantes suelen escribirse en mayúsculas.
+*/
